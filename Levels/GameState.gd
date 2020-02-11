093cea6cb7e2d1d8ae4103d7cdb0a6c7fb6060f8
Greywind <alassea.indilwen@gmail.com>
2020-02-11 13:51:34 +0100

    Add GUI for displaying lives/coins

--- Levels/GameState.gd
+++ Levels/GameState.gd
@@ -1,13 +1,20 @@
 extends Node2D
 
-var lives = 3
+var lives = 5
 
 func _ready():
 	add_to_group("Gamestate")
+	update_GUI()
+	
+
+func update_GUI():
+	get_tree().call_group("GUI", "update_lives", lives)
+	
 
 func hurt():
 	lives -= 1
 	$Player.hurt()
+	update_GUI()
 	if lives < 0:
 		end_game()
 
